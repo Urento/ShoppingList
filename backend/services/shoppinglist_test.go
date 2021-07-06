9bccfadabf6bcadb3097f912829bcd24f1f0a10a
Urento <jan@burzinski.de>
2021-07-06 14:20:04 +0200

    wergfsdrtgbsezve4tfsdft

--- backend/services/shoppinglist_test.go
+++ backend/services/shoppinglist_test.go
@@ -1,29 +1,46 @@
 package services
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
+
+	. "github.com/stretchr/testify/assert"
+	"github.com/urento/shoppinglist/models"
+	"github.com/urento/shoppinglist/pkg/util"
 )
 
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
 func TestCreate(t *testing.T) {
-	id := 3498757245897
-	title := "Test Title öikuhadsfbglhkjdsbnfgjlhdsfbgjhdksfbg"
-	items := []string{"1", "2", "3", "skjdhfgbnsdljhkfgbdkhjsfbgjdhfbgjdhfg"}
-	owner := "Urento hkalsbdrgojuzsdafghkdsfg"
-	participants := []string{"Me", "you", "m123123123e", "38383883"}
-	shoppinglist := Shoppinglist{
+	Setup()
+
+	id := randInt(1000, 50000)
+	title := "title" + StringWithCharset(20)
+	items := []string{StringWithCharset(45), StringWithCharset(45), StringWithCharset(45), StringWithCharset(45)}
+	owner := "owner" + StringWithCharset(30)
+	participants := []string{StringWithCharset(45), StringWithCharset(45), StringWithCharset(45), StringWithCharset(45)}
+	shoppinglist := Shoppi	nglist{
 		ID:           id,
 		Title:        title,
 		Items:        items,
 		Owner:        owner,
 		Participants: participants,
 	}
-	err := shoppinglist.Create()
-	if err != nil {
+	created, err := shoppinglist.Create()
+	if err != nil || !created {
+		fmt.Println(err.Error())
 		t.Errorf("Failed to create shoppinglist")
 	}
 
 	list := Shoppinglist{ID: id}
 	exists, err := list.ExistsByID()
+	t.Log(exists)
 	if err != nil || !exists {
 		t.Errorf("Shoppinglist did not get created")
 	}
@@ -33,20 +50,34 @@ func TestCreate(t *testing.T) {
 		t.Errorf("Shoppinglist not found")
 	}
 
-	if l.ID != id || !testEq(l.Items, items) || l.Owner != owner || !testEq(l.Participants, participants) || l.Title != title {
-		t.Errorf("Values weren't correct ")
-	}
-
+	Equal(t, created, true)
+	Equal(t, id, l.ID)
+	Equal(t, items, l.Items)
+	Equal(t, participants, l.Participants)
+	Equal(t, title, l.Title)
+	Equal(t, owner, l.Owner)
 }
 
-func testEq(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
+func TestCreateAndDelete(t *testing.T) {
+	Setup()
+
+	Equal(t, true, true)
+}
+
+func StringWithCharset(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
 	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
+	return string(b)
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(100000)
+}
+
+func Setup() {
+	models.Setup(true)
+	util.Setup()
 }
